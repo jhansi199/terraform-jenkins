7ced1b2dea0b02ffa038d118f42081332f08ad96
jhansi199 <99714685+jhansi199@users.noreply.github.com>
2022-04-10 21:02:56 +0530

    Update and rename terraform-git-jenkins-aws.tf-ec2.txt to terraform-git-jenkins-aws-ec2.tf

--- terraform-git-jenkins-aws-ec2.tf
+++ terraform-git-jenkins-aws-ec2.tf
@@ -3,7 +3,7 @@ provider "aws"{
 }
 resource "aws instance" "web"{
     count = "1"
-    ami = ""
+    ami = "ami-04505e74c0741db8d"
     instance_type = "t2.micro"
     key_name = "test_kp"
     tags = {
